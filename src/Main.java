c9e28e8d746a30ea21b217c797f615d9d6656e7e
Hemanth Simha <hemanthsimha@Hemanths-MacBook-Air.local>
2022-10-09 22:41:24 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("main() starts.......");
+
+        System.out.println("Hello world!");
+        new Person().run();
+
+        System.out.println("main() ends..........");
+
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,5 @@
+public class Person {
+    public void run(){
+        System.out.println("run 1000 mtr.....");
+    }
+}
